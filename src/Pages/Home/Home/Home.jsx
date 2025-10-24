03ccf80370f82d8fee7455d4a729e8e1cbcfc30e
rahimthis <info.abdurrahim12@gmail.com>
2025-10-24 07:44:11 +0600

    Featured product

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Banner from "../Banner/Banner";
+import ProdcutFeatured from "../../product/ProdcutFeaturd";
 
 const Home = () => {
   return (
     <>
       <Banner />
+      <ProdcutFeatured />
     </>
   );
 };
