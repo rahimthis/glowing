7bf64204e165be9a0d5422c75d426720a25b875a
rahimthis <info.abdurrahim12@gmail.com>
2025-11-24 12:06:32 +0600

    update landign pages

--- client/src/Pages/Home/FeaturesSection/FeaturesSection.jsx
+++ client/src/Pages/Home/FeaturesSection/FeaturesSection.jsx
@@ -1,23 +1,27 @@
 import { FaBox, FaLeaf, FaComments, FaCreditCard } from "react-icons/fa";
+import icon1 from './icon-box-01.svg'
+import icon2 from './icon-box-02.svg'
+import icon3 from './icon-box-03.svg'
+import icon4 from './icon-box-04.svg'
 
 const features = [
   {
-    icon: <FaBox className="text-green-800 text-4xl mb-4" />,
+    icon: icon1 ,
     title: "Free Shipping",
     description: "Free Shipping for orders over $130",
   },
   {
-    icon: <FaLeaf className="text-green-800 text-4xl mb-4" />,
+    icon: icon2,
     title: "Returns",
     description: "Within 30 days for an exchange.",
   },
   {
-    icon: <FaComments className="text-green-800 text-4xl mb-4" />,
+    icon: icon3,
     title: "Online Support",
     description: "24 hours a day, 7 days a week",
   },
   {
-    icon: <FaCreditCard className="text-green-800 text-4xl mb-4" />,
+    icon: icon4,
     title: "Flexible Payment",
     description: "Pay with Multiple Credit Cards",
   },
@@ -29,7 +33,7 @@ const FeaturesSection = () => {
       <div className="max-w-7xl mx-auto px-6 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-12 text-center">
         {features.map((feature, index) => (
           <div key={index}>
-            <div className="flex justify-center">{feature.icon}</div>
+            <div className="flex justify-center"><img src={feature.icon} alt="" /></div>
             <h3 className="text-lg font-semibold text-black mb-2">
               {feature.title}
             </h3>
--- client/src/Pages/Shared/Navbar/Navbar.jsx
+++ client/src/Pages/Shared/Navbar/Navbar.jsx
@@ -22,6 +22,7 @@ const Navbar = () => {
     <nav className="w-full bg-white shadow-sm sticky top-0 z-50">
       <div className="max-w-7xl mx-auto px-4 lg:px-8">
         <div className="flex justify-between items-center h-16">
+          
           {/* LEFT SECTION */}
           <ul className="hidden md:flex items-center space-x-8 font-medium text-sm text-gray-800">
             {navItems.map((item) => {
